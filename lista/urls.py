93f6641ed8edf9b469914635f9cbeac5353276a3
Lucas Meira <lucmeir@gmail.com>
2014-09-18 17:56:59 -0300

    Feito a listagem dos objetos do model Categoria

--- lista/urls.py
+++ lista/urls.py
@@ -5,4 +5,10 @@ from lista import views
 
 urlpatterns = patterns('',
 	url(r'^$', views.index, name='index'),
+	# Categoria
+	url(r'^listarcategorias', views.ListarCategorias.as_view()),
+	# Produto
+	url(r'^listarprodutos', views.ListarProdutos.as_view()),
+	# Lista
+	url(r'^mostrarlista', views.MostrarLista.as_view()),
 )
--- lista/views.py
+++ lista/views.py
@@ -1,9 +1,34 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
+#from django.views.generic.list_detail import object_list
+#from django.views.generic.create_update import create_object, update_object, delete_object
+from django.db.models import get_model
 from django.shortcuts import get_object_or_404, render
+from lista.models import Categoria
+from django.views.generic import ListView
 
 # Create your views here.
 def index(request):
 	#return HttpResponse("Home page Lista")
 	return render(request, 'lista/index.html')
+
+def listar(request, model):
+	#return render(request, 'lista/listar.html')
+	model_object = get_model('categoria', model)
+	if model_object:
+		model_list = model_object.objects.all()
+	else:
+		raise Http404()
+	
+	return object_list(request, queryset=model_list)
+	
+class ListarCategorias(ListView):
+	template_name = 'lista/listarcategorias.html'
+	model = Categoria
+
+class ListarProdutos(ListView):
+	pass
+	
+class MostrarLista(ListView):
+	pass
